c5ced784e81ed4007575a5a16604a3efeaf80770
jhuang10123 <133@holbertonschool.com>
2018-01-03 06:22:27 +0000

    pep8

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,13 +5,14 @@ Export all tasks by all employees in JSON format
 import requests
 import json
 
+
 def get_dict(username, task, status):
     dictionary = {}
     dictionary['username'] = username
     dictionary['task'] = task
     dictionary['completed'] = status
     return dictionary
-    
+
 
 if __name__ == '__main__':
     import requests
@@ -29,14 +30,17 @@ if __name__ == '__main__':
     for item in user_json:
         user_dict[item['id']] = item['username']
 
-
     new_dict = {}
 
     for key in user_dict:
         task_list = []
         for item in todo_all:
             if item['userId'] == key:
-                task_list.append(get_dict(user_dict.get(key), item.get('title'), item.get('completed')))
+                task_list.append(
+                    get_dict(
+                        user_dict.get(key),
+                        item.get('title'),
+                        item.get('completed')))
         new_dict[key] = task_list
 
     filename = "todo_all_employees.json"
