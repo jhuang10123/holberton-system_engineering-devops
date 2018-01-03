aa25999ef1ccc6d248820034bdd0adabc6f8731d
jhuang10123 <133@holbertonschool.com>
2018-01-03 03:16:22 +0000

    task 2 draft

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
-Export tasks owned by an employee in CSV format
+Export tasks owned by an employee in JSON format
 """
 
 
 if __name__ == '__main__':
     import requests
     from sys import argv
+    import json
 
-    user_id = argv[1]
     user_payload = {'id': argv[1]}
     todo_payload = {'userId': argv[1]}
     todo_url = 'https://jsonplaceholder.typicode.com/todos'
@@ -21,13 +21,19 @@ if __name__ == '__main__':
         username = item['username']
 
 # getting employee todo
+    filename = argv[1] + ".json"
+    to_dict = {}
+    json_dict = {}
+    val_list = []
     todo_req = requests.get(todo_url, params=todo_payload)
     todo_json = todo_req.json()
-    csvfile = user_id + ".csv"
-
     for item in todo_json:
         status = item.get('completed')
         title = item.get('title')
-        with open(csvfile, "a+") as output:
-            output.write('"{}","{}","{}","{}"\n'.format
-                         (user_id, username, status, title))
+        to_dict.update(
+            {"task": title, "completed": status, "username": username})
+        val_list.append(to_dict)
+        json_dict = {argv[1]: val_list}
+
+        with open(filename, 'w') as output:
+            json.dump(json_dict, output)
