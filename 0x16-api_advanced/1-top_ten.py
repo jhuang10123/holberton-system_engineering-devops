21acd34d4eb2580b588e711ecc4de876d0d7c166
jhuang10123 <133@holbertonschool.com>
2018-01-08 23:34:02 +0000

    adding exception

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,16 +8,17 @@ from pprint import pprint
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    req = requests.get(
-        url, allow_redirects=False, headers={
-            'User-Agent': 'Jennifer12345'},
-        params={'limit': 10})
-    if not req:
-        print(None)
-    # getting data children from 'data' of req.json()
-    else:
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        req = requests.get(
+            url, allow_redirects=False, headers={
+                'User-Agent': 'Jennifer12345'},
+            params={'limit': 10})
+
+        # getting data children from 'data' of req.json()
         children = req.json().get('data').get('children')
-    # loop through list of dict and print out value of 'title' for each
+        # loop through list of dict and print out value of 'title' for each
         for child in children:
             print(child.get('data').get('title'))
+    except:
+        print(None)
