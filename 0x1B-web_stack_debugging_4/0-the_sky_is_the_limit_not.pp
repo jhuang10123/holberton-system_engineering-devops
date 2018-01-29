07fca2f0b081d50593a5742545a056902c930408
jhuang10123 <133@holbertonschool.com>
2018-01-29 21:22:22 +0000

    adding restart

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,8 @@ exec { 'change-max-open-files':
   command => 'echo "ULIMIT=\"-n 5000\"" > nginx',
   path    => '/usr/local/bin/:/bin/',
 }
+
+exec { 'restart nginx':
+  command => 'service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
