0d367c0eb3e22e7eba8e5317aba7e7eadb51d1df
jhuang10123 <133@holbertonschool.com>
2018-01-29 21:51:24 +0000

    lowering limit, adding sudo

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,10 @@
 exec { 'change-max-open-files':
   cwd     => '/etc/default/',
-  command => 'sudo sed -i "s/15/15000/" nginx',
+  command => 'sudo sed -i "s/15/5000/" nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 exec { 'restart nginx':
-  command => 'service nginx restart',
+  command => 'sudo service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
