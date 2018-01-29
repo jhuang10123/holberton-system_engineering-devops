618814931221f00ff3080860b67e1135e54f51d9
jhuang10123 <133@holbertonschool.com>
2018-01-29 21:39:10 +0000

    replacing with sed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 exec { 'change-max-open-files':
   cwd     => '/etc/default/',
-  command => 'echo "ULIMIT=\"-n 5000\"" > nginx',
-  path    => '/usr/local/bin/:/bin/',
+  command => 'sudo sed -i "s/15/15000/" nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
 exec { 'restart nginx':
