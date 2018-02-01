feb610140606d753b1c2784ca98a59087bfe88da
jhuang10123 <133@holbertonschool.com>
2018-02-01 03:27:57 +0000

    rm second exec

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
 # changing max open file limit
 exec { 'change-max-open-files':
-  command => 'sudo sed -i "s/15/5000/" /etc/default/nginx',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-}
-
-exec { 'restart nginx':
-  command => 'sudo service nginx restart',
+  command => 'sudo sed -i "s/15/5000/" /etc/default/nginx; service nginx restart',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
